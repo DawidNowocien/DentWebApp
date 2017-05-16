8cc34035821c17466cca123962159eeb3c109cf7
DawidNowocien <dawid.nowocien@gmail.com>
2017-05-17 01:04:49 +0200

    Initial angularJs config

--- src/main/java/pl/dentsys/Application.java
+++ src/main/java/pl/dentsys/Application.java
@@ -2,7 +2,6 @@ package pl.dentsys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 //@Import(JpaConfiguration.class)
 @SpringBootApplication(scanBasePackages={"pl.dentsys"})
