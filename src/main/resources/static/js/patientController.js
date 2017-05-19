b3d06f2c33cdfb23af755fc83c8593f2888e0fe4
MarcinNowak1 <cinek601@gmail.com>
2017-05-19 13:27:52 +0200

    zabawa z patientController

--- src/main/resources/static/js/patientController.js
+++ src/main/resources/static/js/patientController.js
@@ -1,7 +1,41 @@
-app.controller('usersController', function($scope) {
-    $scope.headingTitle = "User List";
-});
+//1. create app module 
+    var app = angular.module('app', []);
 
-app.controller('rolesController', function($scope) {
-    $scope.headingTitle = "Roles List";
+    //2. create controller
+    app.controller("patientController", function ($scope, $http) {
+
+        //3. attach originalStudent model object
+        $scope.originalnewPatient = {
+            name: 'James',
+            surname: 'Bond',
+            dateBirth: new Date('1980/01/31'),
+            address: 'Górczewska 6'
+        };
+
+        //4. copy originalStudent to student. student will be bind to a form 
+        $scope.newPatient = angular.copy($scope.originalnewPatient);
+
+        //5. create submitStudentForm() function. This will be called when user submits the form
+        $scope.submitPatientForm = function () {
+
+            var onSuccess = function (data, status, headers, config) {
+                alert('Patient saved successfully.');
+            };
+
+            var onError = function (data, status, headers, config) {
+                alert('Error occured.');
+            }
+
+            $http.post('/student/submitData', { newPatient:$scope.newPatient })
+                .success(onSuccess)
+                .error(onError);
+            $window.location.href = 'test';
+
+        };
+
+        //6. create resetForm() function. This will be called on Reset button click.  
+        $scope.resetForm = function () {
+            $scope.newPatient = angular.copy($scope.originalnewPatient);
+        };
 });
+  
