688811a137c415d7e1ab7deb2285212be362397f
MarcinNowak1 <cinek601@gmail.com>
2017-05-28 19:31:00 +0200

    dodanie doctorController

--- src/main/resources/static/js/doctorController.js
+++ src/main/resources/static/js/doctorController.js
@@ -2,21 +2,21 @@
     var app = angular.module('app', []);
 
     //2. create controller
-    app.controller("patientController", function ($scope, $http) {
+    app.controller("doctorController", function ($scope, $http) {
 
-        //3. attach originalPatient model object
-        $scope.originalnewPatient = {
+        //3. attach originalDoctor model object
+        $scope.originalnewDoctor = {
             name: 'James',
             surename: 'Bond',
             dateBirth: new Date('1980/01/31'),
             address: 'Górczewska 6'
         };
 
-        //4. copy originalnewPatient to newPatient. newPatient will be bind to a form 
-        $scope.newPatient = angular.copy($scope.originalnewPatient);
+        //4. copy originalnewDoctor to newDoctor. newDoctor will be bind to a form 
+        $scope.newDoctor = angular.copy($scope.originalnewDoctor);
 
-        //5. create submitnewPatientForm() function. This will be called when user submits the form
-        $scope.submitPatientForm = function () {
+        //5. create submitnewDoctorForm() function. This will be called when user submits the form
+        $scope.submitDoctorForm = function () {
 
             var onSuccess = function (data, status, headers, config) {
                 alert('Patient saved successfully.');
@@ -27,7 +27,7 @@
                 alert('Error occured.');
             }
 
-            $http.post('add-patient', $scope.newPatient)
+            $http.post('add-doctor', $scope.newDoctor)
                 .success(onSuccess)
                 .error(onError);
 //            $window.location.href = '/test.html';
@@ -36,7 +36,7 @@
 
         //6. create resetForm() function. This will be called on Reset button click.  
         $scope.resetForm = function () {
-            $scope.newPatient = angular.copy($scope.originalnewPatient);
+            $scope.newDoctor = angular.copy($scope.originalnewDoctor);
         };
 });
   
