a2d2dd4810d35aadbd7c85b877a3998cc612c06c
MarcinNowak1 <cinek601@gmail.com>
2017-06-04 10:07:18 +0200

    dodanie editvisit

---
+++ src/main/resources/static/js/editVisitController.js
@@ -0,0 +1,38 @@
+//1. create app module 
+    var app = angular.module('app', []);
+
+    //2. create controller
+    app.controller("editVisitController", function ($scope, $http) {
+
+        //3. attach originalvisit model object
+        $scope.originaleditVisit = {
+        
+        };
+
+        //4. copy originalnewVisit to newVisit. newVisit will be bind to a form 
+        $scope.editVisit = angular.copy($scope.originaleditVisit);
+
+        //5. create submitnewVisitForm() function. This will be called when user submits the form
+        $scope.submitEditVisitForm = function () {
+
+            var onSuccess = function (data, status, headers, config) {
+                alert('Visit saved successfully.');
+                
+            };
+
+            var onError = function (data, status, headers, config) {
+                alert('Error occured.');
+            }
+
+            $http.post('edit-visit', $scope.editVisit)
+                .success(onSuccess)
+                .error(onError);
+            window.location.replace('/startDoctorPage.html');
+        };
+
+        //6. create resetForm() function. This will be called on Reset button click.  
+        $scope.resetForm = function () {
+            $scope.editVisit = angular.copy($scope.originaleditVisit);
+        };
+});
+  
