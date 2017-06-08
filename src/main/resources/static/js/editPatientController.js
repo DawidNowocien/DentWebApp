b4b587f465052ce087208709eaee256ca37cd635
MarcinNowak1 <cinek601@gmail.com>
2017-06-08 22:56:24 +0200

    poprawa editPatient

--- src/main/resources/static/js/editPatientController.js
+++ src/main/resources/static/js/editPatientController.js
@@ -25,24 +25,24 @@
     	 //$scope.originaleditPatient = $http.get('data/dane.json');
     	
         //4. copy originalnewPatient to newPatient. newPatient will be bind to a form 
-        $scope.editPatient = angular.copy($scope.originaleditPatient);
+        //$scope.addPatient = angular.copy($scope.originaleditPatient);
 
         //5. create submitnewPatientForm() function. This will be called when user submits the form
         $scope.submitEditPatientForm = function () {
 
         
-            var onSuccess = function (data, status, headers, config) {
-            	alert('Dane pacjenta zostały prawidłowo zapisane.');
-            	
-                             
-            };
-
-            var onError = function (data, status, headers, config) {
-                alert('Wystąpił błąd podczas zapisywania danych.');
-            }
-            $http.put('patient/'+searchObject_2, $scope.editPatient)
-            .success(onSuccess)
-            .error(onError); 
+//            var onSuccess = function (data, status, headers, config) {
+//            	alert('Dane pacjenta zostały prawidłowo zapisane.');
+//            	
+//                             
+//            };
+//
+//            var onError = function (data, status, headers, config) {
+//                alert('Wystąpił błąd podczas zapisywania danych.');
+//            }
+            $http.put('patient/'+searchObject_2, $scope.originaleditPatient);
+//            .success(onSuccess)
+//            .error(onError); 
             window.location.replace('/startDoctorPage.html');
  
         };
