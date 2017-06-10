c0567edeaa53c573b5bf87b960047a78e3007170
DawidNowocien <dawid.nowocien@gmail.com>
2017-06-10 15:36:02 +0200

    Revert "Security configuration."
    This reverts commit b81ac2fd0c7f30b5c8b865b0d49efe1cc3d90d61.

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into app_user(id,name,username,password) values (1,'Dawid Nowocień','admin','admin');
-insert into app_user(id,name,username,password) values (2,'Marcin Nowak','user','user');
-
-insert into app_user_roles(app_user_id,roles) values (1,'ADMIN');
-insert into app_user_roles(app_user_id,roles) values (1,'USER');
-insert into app_user_roles(app_user_id,roles) values (2,'USER');
