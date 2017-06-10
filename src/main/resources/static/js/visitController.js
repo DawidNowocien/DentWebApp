0c709ebd4d64ab53237e361e555682b6426c0da1
MarcinNowak1 <cinek601@gmail.com>
2017-06-10 10:04:45 +0200

    addvisit

--- src/main/resources/static/js/visitController.js
+++ src/main/resources/static/js/visitController.js
@@ -1,13 +1,87 @@
 //1. create app module 
     var app = angular.module('app', []);
 
-    
-    
-    
+
+    app.service("VisitService",function($http, $q,$window, $location)
+    { 	 
+    	
+    	 
+    	var deferred = $q.defer();
+    	$http.get('treatment/dict').then(function(data)
+    	{
+    		deferred.resolve(data);
+    	});
+
+    	this.getStations = function ()
+    	{
+    		return deferred.promise;
+    	}
+
+    	})
+
+    	
+    	app.service("PatientService",function($http, $q,$window, $location)
+    { 	 
+    	
+    	 
+    	var deferred = $q.defer();
+    	$http.get('patient-list').then(function(data)
+    	{
+    		deferred.resolve(data);
+    	});
+
+    	this.getPatients = function ()
+    	{
+    		return deferred.promise;
+    	}
+
+    	})
+    	
+    	app.service("DoctorService",function($http, $q,$window, $location)
+    { 	 
+    	
+    	 
+    	var deferred = $q.defer();
+    	$http.get('doctor-list').then(function(data)
+    	{
+    		deferred.resolve(data);
+    	});
+
+    	this.getDoctors = function ()
+    	{
+    		return deferred.promise;
+    	}
+
+    	})
+
     
     //2. create controller
-    app.controller("visitController", function ($scope, $http) {
+    app.controller("visitController", function ($scope, $http, VisitService,PatientService,DoctorService, $window) {
 
+    	
+    	
+      	var promise = VisitService.getStations();
+    	promise.then(function(data)
+    	{
+    		$scope.stations = data.data;
+
+    	});
+    	
+    	var promise_2 = PatientService.getPatients();
+    	promise_2.then(function(data)
+    	{
+    		$scope.patients = data.data;
+
+    	});
+    	
+    	var promise_3 = DoctorService.getDoctors();
+    	promise_3.then(function(data)
+    	{
+    		$scope.doctors = data.data;
+
+    	});
+    	
+    	//$scope.dateMeet = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/ /^\+?\d{2}[- ]?\d{2}$/;
         //3. attach originalvisit model object
         $scope.originalnewVisit = {
         		
@@ -29,7 +103,7 @@
                 alert('Wystąpił błąd podczas zapisywania danych.');
             }
 
-            $http.post('add-visit', $scope.newVisit)
+            $http.post('visit', $scope.newVisit)
                 .success(onSuccess)
                 .error(onError);
             window.location.replace('/startDoctorPage.html');
